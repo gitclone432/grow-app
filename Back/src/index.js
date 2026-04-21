73f4a788c5d9f776b17989b7db41a28ae03377ef
gitclone432 <gitclone432@gmail.com>
2026-04-21 23:56:25 +0530

    short commit title

--- Back/src/index.js
+++ Back/src/index.js
@@ -83,6 +83,8 @@ app.use(helmet());
 const ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'http://127.0.0.1:5173',
+  'http://127.0.0.1:3000',
   ...((process.env.CLIENT_ORIGIN || '')
     .split(',')
     .map(o => o.trim())
